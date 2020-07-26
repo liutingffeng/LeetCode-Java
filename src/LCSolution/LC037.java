0e7724b81799d0112425301f749c7b518e3f6ca2
ltf <616588738@qq.com>
2020-07-26 10:52:45 +0800

    add

--- src/LCSolution/LC037.java
+++ src/LCSolution/LC037.java
@@ -6,62 +6,178 @@ import java.util.Map;
 public class LC037 {
 
     class Solution {
-        public void solveSudoku(char[][] board) {
-            boolean[][] row = new boolean[9][10];
-            boolean[][] col = new boolean[9][10];
-            boolean[][] block = new boolean[9][10];
+//        public void solveSudoku(char[][] board) {
+//            boolean[][] row = new boolean[9][10];
+//            boolean[][] col = new boolean[9][10];
+//            boolean[][] block = new boolean[9][10];
+//
+//            //初始化
+//            for (int i=0;i<9;i++)
+//                for (int j=0;j<9;j++){
+//                    if (board[i][j]!='.'){
+//                        row[i][board[i][j]-'0'] = true;
+//                        col[j][board[i][j]-'0'] = true;
+//                        block[i/3*3+j/3][board[i][j]-'0'] = true;
+//                    }
+//                }
+//
+//            for (int i=0;i<81;i++){
+//                if(board[i / 9][i % 9] == '.'){
+//                    dfs(board, i, row, col, block);
+//                    return;
+//                }
+//            }
+//        }
+//
+//        private boolean dfs(char[][] board,int count,boolean[][] row,boolean[][] col,boolean[][] block){
+//
+//            if (count>=81)
+//                return true;
+//
+//            int nexpos = count+1;
+//            for (;nexpos<81;nexpos++){
+//                if (board[nexpos/9][nexpos%9] == '.')
+//                    break;
+//            }
+//
+//            int x = count/9, y = count%9;
+//            for(char i = 1; i <= 9; i ++)
+//                if(!row[x][i] && !col[y][i] && !block[x / 3 * 3 + y / 3][i]){
+//                    row[x][i] = true;
+//                    col[y][i] = true;
+//                    block[x / 3 * 3 + y / 3][i] = true;
+//                    board[x][y] = String.valueOf(i).toCharArray()[0];
+//
+//                    if(dfs(board, nexpos, row, col, block))
+//                        return true;
+//
+//                    row[x][i] = false;
+//                    col[y][i] = false;
+//                    block[x / 3 * 3 + y / 3][i] = false;
+//                    board[x][y] = '.';
+//                }
+//            return false;
+//
+//        }
 
-            //初始化
+
+        private boolean[][] row ;
+        private boolean[][] col;
+        int m;
+        int n;
+        public void solveSudoku(char[][] board) {
+            m = 9;
+            n = 9;
+            row = new boolean[9][10];
+            col = new boolean[9][10];
+//初始化
             for (int i=0;i<9;i++)
                 for (int j=0;j<9;j++){
                     if (board[i][j]!='.'){
                         row[i][board[i][j]-'0'] = true;
                         col[j][board[i][j]-'0'] = true;
-                        block[i/3*3+j/3][board[i][j]-'0'] = true;
                     }
                 }
-
-            for (int i=0;i<81;i++){
-                if(board[i / 9][i % 9] == '.'){
-                    dfs(board, i, row, col, block);
-                    return;
-                }
-            }
+            recusion(board, 0, 0);
+//            backtrack(board, 0, 0);
         }
 
-        private boolean dfs(char[][] board,int count,boolean[][] row,boolean[][] col,boolean[][] block){
+        private boolean recusion(char[][] board,int x,int y){
 
-            if (count>=81)
+            // 这一行算完了
+            if (y == n){
+                return recusion(board, x+1, 0);
+            }
+
+            // 结束
+            if (x == m){
                 return true;
-
-            int nexpos = count+1;
-            for (;nexpos<81;nexpos++){
-                if (board[nexpos/9][nexpos%9] == '.')
-                    break;
             }
 
-            int x = count/9, y = count%9;
-            for(char i = 1; i <= 9; i ++)
-                if(!row[x][i] && !col[y][i] && !block[x / 3 * 3 + y / 3][i]){
-                    row[x][i] = true;
-                    col[y][i] = true;
-                    block[x / 3 * 3 + y / 3][i] = true;
-                    board[x][y] = String.valueOf(i).toCharArray()[0];
+            if (board[x][y] != '.'){
+                return recusion(board,x,y+1);
+            }
 
-                    if(dfs(board, nexpos, row, col, block))
-                        return true;
-
-                    row[x][i] = false;
-                    col[y][i] = false;
-                    block[x / 3 * 3 + y / 3][i] = false;
-                    board[x][y] = '.';
+            for (char c = '1';c<='9';c++){
+                if (!isValid(board,x,y,c)){
+                    continue;
                 }
+                //找到了一个该位置的解
+                board[x][y] = c;
+                row[x][c-'0'] = true;
+                col[y][c-'0'] = true;
+                if (recusion(board, x, y+1))
+                    return true;
+                row[x][c-'0'] = false;
+                col[y][c-'0'] = false;
+                board[x][y] = '.';
+            }
             return false;
-
         }
+
+        private boolean isValid(char[][] board, int x, int y,char c) {
+            int num = c-'0';
+            if (row[x][num])
+                return false;
+
+            if (col[y][num])
+                return false;
+
+            // 块
+            for (int i = 0; i < 9; i++) {
+                if (board[(x/3)*3+i/3][(y/3)*3+i%3] == c)
+                    return false;
+            }
+            return true;
+        }
+
+//        boolean backtrack(char[][] board, int i, int j) {
+//            int m = 9, n = 9;
+//            if (j == n) {
+//                // 穷举到最后一列的话就换到下一行重新开始。
+//                return backtrack(board, i + 1, 0);
+//            }
+//            if (i == m) {
+//                // 找到一个可行解，触发 base case
+//                return true;
+//            }
+//
+//            if (board[i][j] != '.') {
+//                // 如果有预设数字，不用我们穷举
+//                return backtrack(board, i, j + 1);
+//            }
+//
+//            for (char ch = '1'; ch <= '9'; ch++) {
+//                // 如果遇到不合法的数字，就跳过
+//                if (!isValid(board, i, j, ch))
+//                    continue;
+//
+//                board[i][j] = ch;
+//                // 如果找到一个可行解，立即结束
+//                if (backtrack(board, i, j + 1)) {
+//                    return true;
+//                }
+//                board[i][j] = '.';
+//            }
+//            // 穷举完 1~9，依然没有找到可行解，此路不通
+//            return false;
+//        }
+
+//        boolean isValid(char[][] board, int r, int c, char n) {
+//            for (int i = 0; i < 9; i++) {
+//                // 判断行是否存在重复
+//                if (board[r][i] == n) return false;
+//                // 判断列是否存在重复
+//                if (board[i][c] == n) return false;
+//                // 判断 3 x 3 方框是否存在重复
+//                if (board[(r/3)*3 + i/3][(c/3)*3 + i%3] == n)
+//                    return false;
+//            }
+//            return true;
+//        }
     }
 
     public static void main(String[] args) {
-        System.out.println('0'+1 );
+        System.out.println('1'-'0');
     }
 }
--- src/LCSolution/LC224.java
+++ src/LCSolution/LC224.java
@@ -17,90 +17,234 @@ public class LC224 {
     class Solution {
 
         //"(1+(4+5+2)-3)+(6+8)"
-        public int calculate(String s) {
-            final int STATE_BEGIN = 0;
-            final int NUMBER_STATE = 1;
-            final int OPERATION_STATE = 2;//分别定义开始状态，数字状态，操作符状态
-            Deque<Long> numStack = new ArrayDeque<>();
-            Deque<Character> opStack = new ArrayDeque<>();//分别定义数字栈，操作符栈
-            long number = 0;//坑爹leetcode 由于存在超级大的数字，这里用long，int不够
-            int STATE = STATE_BEGIN;
-            int compuate_flag = 0;//初始化字符转数字初始值，初始状态，计算标志位
-            for (int i = 0; i < s.length(); i++){
-                if (s.charAt(i) == ' '){
-                    continue;
-                }
-                switch (STATE){
-                    case STATE_BEGIN: // 开始状态
-                        if (s.charAt(i) >= '0' && s.charAt(i)<='9'){
-                            STATE = NUMBER_STATE;//如果遇到数字，转到数字状态
-                        }
-                        else {
-                            STATE = OPERATION_STATE;//遇到其他东西，转到操作符状态
-                        }
-                        i--;//退格是因为转到了对应状态没处理呀，所以退回来要处理
-                        break;
+//        public int calculate(String s) {
+//            final int STATE_BEGIN = 0;
+//            final int NUMBER_STATE = 1;
+//            final int OPERATION_STATE = 2;//分别定义开始状态，数字状态，操作符状态
+//            Deque<Long> numStack = new ArrayDeque<>();
+//            Deque<Character> opStack = new ArrayDeque<>();//分别定义数字栈，操作符栈
+//            long number = 0;//坑爹leetcode 由于存在超级大的数字，这里用long，int不够
+//            int STATE = STATE_BEGIN;
+//            int compuate_flag = 0;//初始化字符转数字初始值，初始状态，计算标志位
+//            for (int i = 0; i < s.length(); i++){
+//                if (s.charAt(i) == ' '){
+//                    continue;
+//                }
+//                switch (STATE){
+//                    case STATE_BEGIN: // 开始状态
+//                        if (s.charAt(i) >= '0' && s.charAt(i)<='9'){
+//                            STATE = NUMBER_STATE;//如果遇到数字，转到数字状态
+//                        }
+//                        else {
+//                            STATE = OPERATION_STATE;//遇到其他东西，转到操作符状态
+//                        }
+//                        i--;//退格是因为转到了对应状态没处理呀，所以退回来要处理
+//                        break;
+//
+//                    case NUMBER_STATE: //数字状态
+//                        if (s.charAt(i) >= '0' && s.charAt(i)<='9'){
+//                            number = number*10+s.charAt(i)-'0';
+//                        }
+//                        else {// 代表数字处理完了
+//                            //数字入栈
+//                            numStack.addLast(number);
+//                            if (compuate_flag == 1){
+//                                compute(numStack, opStack);//如果计算标识符为1，就可以计算
+//                            }
+//                            number = 0;
+//                            i--;//凡是状态内部处理并且发生状态转移之后，都要退格，状态转移后要重新扫描处理
+//                            STATE = OPERATION_STATE;
+//                        }
+//                        break;
+//                    case OPERATION_STATE:
+//                        if (s.charAt(i) == '+' || s.charAt(i) == '-'){
+//                            opStack.addLast(s.charAt(i));
+//                            compuate_flag = 1;//遇到操作符，压入操作符栈，计算标识符置1
+//                        }
+//                        else if (s.charAt(i) == '('){
+//                            STATE = NUMBER_STATE;
+//                            //不用退格
+//                            compuate_flag = 0;//遇到左括号，先算左括号，转移到数字状态，计算标识符置0
+//                        }
+//                        else if (s.charAt(i)>='0' && s.charAt(i)<='9'){
+//                            STATE = NUMBER_STATE;
+//                            i--;
+//                        }
+//                        else if (s.charAt(i) == ')'){
+//                            compute(numStack, opStack);
+//                        }
+//                        break;
+//                }
+//            }
+//            if (number!=0){
+//                numStack.addLast(number);
+//                compute(numStack, opStack);
+//                     //最后一步收尾的计算，勿忘！！！
+//            }
+//            if (number == 0 && numStack.isEmpty()){//如果数字值为0且数字栈为空 返回
+//                return 0;
+//            }
+//            return numStack.peekLast().intValue();
+//        }
+//
+//        private void compute(Deque<Long> numStack, Deque<Character> opStack) {
+//            if (numStack.size()<2)
+//                return;
+//
+//            long num2 = numStack.pollLast();
+//            long num1 = numStack.pollLast();
+//            char op = opStack.pollLast();
+//            if (op == '+'){
+//                numStack.addLast(num1+num2);
+//            }
+//            if (op == '-'){
+//                numStack.addLast(num1-num2);
+//            }
+//        }
 
-                    case NUMBER_STATE: //数字状态
-                        if (s.charAt(i) >= '0' && s.charAt(i)<='9'){
-                            number = number*10+s.charAt(i)-'0';
-                        }
-                        else {// 代表数字处理完了
-                            //数字入栈
-                            numStack.addLast(number);
-                            if (compuate_flag == 1){
-                                compute(numStack, opStack);//如果计算标识符为1，就可以计算
-                            }
-                            number = 0;
-                            i--;//凡是状态内部处理并且发生状态转移之后，都要退格，状态转移后要重新扫描处理
-                            STATE = OPERATION_STATE;
-                        }
-                        break;
-                    case OPERATION_STATE:
-                        if (s.charAt(i) == '+' || s.charAt(i) == '-'){
-                            opStack.addLast(s.charAt(i));
-                            compuate_flag = 1;//遇到操作符，压入操作符栈，计算标识符置1
-                        }
-                        else if (s.charAt(i) == '('){
-                            STATE = NUMBER_STATE;
-                            //不用退格
-                            compuate_flag = 0;//遇到左括号，先算左括号，转移到数字状态，计算标识符置0
-                        }
-                        else if (s.charAt(i)>='0' && s.charAt(i)<='9'){
-                            STATE = NUMBER_STATE;
-                            i--;
-                        }
-                        else if (s.charAt(i) == ')'){
-                            compute(numStack, opStack);
-                        }
-                        break;
-                }
-            }
-            if (number!=0){
-                numStack.addLast(number);
-                compute(numStack, opStack);
-                     //最后一步收尾的计算，勿忘！！！
-            }
-            if (number == 0 && numStack.isEmpty()){//如果数字值为0且数字栈为空 返回
-                return 0;
-            }
-            return numStack.peekLast().intValue();
+//        public int calculate(String s) {
+//            char[] array = s.toCharArray();
+//            Deque<Integer> nums = new ArrayDeque<>();
+//            Deque<Character> op = new ArrayDeque<>();
+//
+//            int num = -1;
+//            for (int i = 0; i < s.length(); i++) {
+//                if (array[i] == ' ')
+//                    continue;
+//
+//                if (isNumber(array[i])){
+//                    if (num == -1)
+//                        num = 0;
+//                    num = num*10+array[i]-'0';
+//                }
+//                else {
+//                    // 将数字入栈
+//                    if (num!=-1) {
+//                        nums.addLast(num);
+//                        num = -1;
+//                    }
+//
+//                    if (isOperate(array[i])){
+//                        while (!op.isEmpty()){
+//                            if (op.peekLast() == '('){
+//                                break;
+//                            }
+//                            int num1 = nums.pollLast();
+//                            int num2 = nums.pollLast();
+//                            if (op.pollLast() == '+'){
+//                                nums.addLast(num2+num1);
+//                            }
+//                            else {
+//                                nums.addLast(num2-num1);
+//                            }
+//                        }
+//
+//                        op.addLast(array[i]);
+//                    }
+//                    else {
+//                        if (array[i] == '(')
+//                            op.addLast(array[i]);
+//                        else {
+//                            while (op.peekLast() != '('){
+//                                int num1 = nums.pollLast();
+//                                int num2 = nums.pollLast();
+//                                if (op.pollLast() == '+'){
+//                                    nums.addLast(num2+num1);
+//                                }
+//                                else {
+//                                    nums.addLast(num2-num1);
+//                                }
+//                            }
+//                            op.pollLast();
+//                        }
+//                    }
+//                }
+//            }
+//
+//            if (num!=-1) {
+//                nums.addLast(num);
+//            }
+//            while (!op.isEmpty()){
+//                int num1 = nums.pollLast();
+//                int num2 = nums.pollLast();
+//                if (op.pollLast() == '+'){
+//                    nums.addLast(num2+num1);
+//                }
+//                else {
+//                    nums.addLast(num2-num1);
+//                }
+//            }
+//            return nums.pollLast();
+//        }
+
+        private boolean isNumber(char c){
+            return c>='0' && c<='9';
         }
 
-        private void compute(Deque<Long> numStack, Deque<Character> opStack) {
-            if (numStack.size()<2)
-                return;
-
-            long num2 = numStack.pollLast();
-            long num1 = numStack.pollLast();
-            char op = opStack.pollLast();
-            if (op == '+'){
-                numStack.addLast(num1+num2);
-            }
-            if (op == '-'){
-                numStack.addLast(num1-num2);
-            }
+        private boolean isOperate(char c){
+            return c == '+' || c == '-' || c == '*' || c == '/';
         }
 
+        int i;
+         //(1+(4+5*2)-3)+(6+8)
+        public int calculate(String s){
+            i = 0;
+            return parse(s);
+        }
+
+        private int parse(String s){
+            Deque<Integer> deque = new ArrayDeque<>();
+            int num = 0;
+            //
+            char sign = '+';
+            for (;i<s.length() && sign!=')';i++){
+                char c = s.charAt(i);
+
+                if (isNumber(c)){
+                    num = num *10 +(c-'0');
+                }
+
+                if (c == '(') {
+                    i++;
+                    num = parse(s);
+                    c = s.charAt(i);
+                }
+
+
+                if ((!isNumber(c) && c!=' ') || i == s.length()-1){
+                    int pre;
+                    switch (sign){
+                        case '+':
+                            deque.addLast(num);
+                            break;
+                        case '-':
+                            deque.addLast(-num);
+                            break;
+                        case '*':
+                            pre = deque.pollLast();
+                            deque.addLast(pre*num);
+                            break;
+                        case '/':
+                            pre = deque.pollLast();
+                            deque.addLast(pre/num);
+                            break;
+                    }
+                    sign = c;
+                    num = 0;
+                }
+            }
+
+            int res = 0;
+            while (!deque.isEmpty()){
+                res += deque.pollLast();
+            }
+            return res;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new LC224().new Solution();
+        System.out.println(solution.calculate("(1+(4+5*2)*3)+(6/8))"));
     }
 }
