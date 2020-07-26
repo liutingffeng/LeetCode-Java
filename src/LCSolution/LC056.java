64b1db37517f7cf8cbc41824f0511b0445357d25
ltf <616588738@qq.com>
2020-07-26 23:28:39 +0800

    learn

--- src/LCSolution/LC056.java
+++ src/LCSolution/LC056.java
@@ -1,28 +1,43 @@
 package LCSolution;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class LC056 {
 
     class Solution {
+//        public int[][] merge(int[][] intervals) {
+//            //左端点从小到大排序
+//            Arrays.sort(intervals, (v1,v2)->v1[0]-v2[0]);
+//            int[][] res = new int[intervals.length][2];
+//            int index = -1;
+//            for (int[] element : intervals){
+//                //如果res中为空，或者当前区间的大于结果集中最大的下标，直接加入，不用合并
+//                if (index == -1 || (element[0] > res[index][1])){
+//                    res[++index] = element;
+//                }
+//                //合并区间
+//                else {
+//                    res[index][1] = Math.max(res[index][1],element[1]);
+//                }
+//            }
+//            return Arrays.copyOf(res, index+1);
+//        }
+
         public int[][] merge(int[][] intervals) {
-            //左端点从小到大排序
-            Arrays.sort(intervals, (v1,v2)->v1[0]-v2[0]);
-            int[][] res = new int[intervals.length][2];
-            int index = -1;
-            for (int[] element : intervals){
-                //如果res中为空，或者当前区间的大于结果集中最大的下标，直接加入，不用合并
-                if (index == -1 || (element[0] > res[index][1])){
-                    res[++index] = element;
+            //
+            Arrays.sort(intervals, (o1,o2)->o1[0]-o2[0]);
+            List<int[]> res = new ArrayList<>();
+            for (int[] interval : intervals){
+                if (res.size() == 0 || interval[0]>res.get(res.size()-1)[1]){
+                    res.add(interval);
                 }
-                //合并区间
                 else {
-                    res[index][1] = Math.max(res[index][1],element[1]);
+                    int[] ele = res.get(res.size()-1);
+                    ele[1] = Math.max(ele[1],interval[1]);
                 }
             }
-            return Arrays.copyOf(res, index+1);
+
+            return res.toArray(new int[0][0]);
         }
     }
 }
--- src/LCSolution/LC560.java
+++ src/LCSolution/LC560.java
@@ -6,21 +6,40 @@ import java.util.Map;
 public class LC560 {
 
     class Solution {
+
+        // 前缀和
+//        public int subarraySum(int[] nums, int k) {
+//
+//             Map<Integer,Integer> map = new HashMap<>();
+//             map.put(0, 1);
+//
+//             int count = 0;
+//             int sum = 0;
+//             for (int num:nums){
+//                 sum +=num;
+//                 if (map.containsKey(sum-k))
+//                     count +=map.get(sum-k);
+//
+//                 map.put(sum, map.getOrDefault(sum, 0)+1);
+//             }
+//             return count;
+//        }
+
         public int subarraySum(int[] nums, int k) {
+            Map<Integer,Integer> map = new HashMap<>();
+            map.put(0,1);
 
-             Map<Integer,Integer> map = new HashMap<>();
-             map.put(0, 1);
+            int res = 0;
+            int sum = 0;
+            for (int num:nums){
+                sum +=num;
+                if (map.containsKey(sum-k)){
+                    res += map.get(sum-k);
+                }
 
-             int count = 0;
-             int sum = 0;
-             for (int num:nums){
-                 sum +=num;
-                 if (map.containsKey(sum-k))
-                     count +=map.get(sum-k);
-
-                 map.put(sum, map.getOrDefault(sum, 0)+1);
-             }
-             return count;
+                map.put(sum, map.getOrDefault(sum, 0)+1);
+            }
+            return res;
         }
     }
 }
