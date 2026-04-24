8e19f2787fbdde54a3a2eeb8d92380272cf7cd2b
LTF <616588738@qq.com>
2026-04-25 00:24:12 +0800

    feat：kt 算法

---
+++ srcKotlin/LCKotlin/LC0.kt
@@ -0,0 +1,31 @@
+package LCKotlin
+
+/**
+ * 1. Two Sum
+ * 给定一个整数数组 nums 和一个整数目标值 target，
+ * 请你在该数组中找出和为目标值 target 的那两个整数，并返回它们的数组下标。
+ */
+class LC0 {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map = HashMap<Int, Int>()
+        for ((i, num) in nums.withIndex()) {
+            val complement = target - num
+            if (map.containsKey(complement)) {
+                return intArrayOf(map[complement]!!, i)
+            }
+            map[num] = i
+        }
+        return intArrayOf()
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val solution = LC0()
+            val nums = intArrayOf(2, 7, 11, 15)
+            val target = 9
+            val result = solution.twoSum(nums, target)
+            println("结果: [${result.joinToString(", ")}]")  // 输出: [0, 1]
+        }
+    }
+}
--- srcKotlin/LCKotlin/LC001.kt
+++ srcKotlin/LCKotlin/LC001.kt
@@ -1,31 +1,21 @@
 package LCKotlin
 
-/**
- * 1. Two Sum
- * 给定一个整数数组 nums 和一个整数目标值 target，
- * 请你在该数组中找出和为目标值 target 的那两个整数，并返回它们的数组下标。
- */
 class LC001 {
-    fun twoSum(nums: IntArray, target: Int): IntArray {
-        val map = HashMap<Int, Int>()
-        for ((i, num) in nums.withIndex()) {
-            val complement = target - num
-            if (map.containsKey(complement)) {
-                return intArrayOf(map[complement]!!, i)
-            }
-            map[num] = i
-        }
-        return intArrayOf()
-    }
 
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            val solution = LC001()
-            val nums = intArrayOf(2, 7, 11, 15)
-            val target = 9
-            val result = solution.twoSum(nums, target)
-            println("结果: [${result.joinToString(", ")}]")  // 输出: [0, 1]
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        var map: MutableMap<Int, Int> = HashMap();
+        for (i in nums.indices) {
+            map.put(nums[i], i)
         }
+        var res: IntArray = IntArray(2)
+        for (i in nums.indices) {
+            val key = target - nums[i]
+            if (map.containsKey(key) && map[key] != i) {
+                res[0] = i
+                res[1] = map[key]!!
+                return res
+            }
+        }
+        return res
     }
 }
